4314ec1334d21d1b90c0045a224daed55a543871
cos1263 <cos1263@DESKTOP-O3KU064>
2020-01-21 18:30:55 +0900

    setterの作成

--- src/main/java/com/example/demo/ReceptionService.java
+++ src/main/java/com/example/demo/ReceptionService.java
@@ -11,17 +11,17 @@ import com.mongodb.client.MongoClients;
 
 @Service
 public class ReceptionService {
-	String	name;
-	String	org;
-	Integer	number;
-	Date	date;
+	String name;
+	String org;
+	Integer number;
+	Date date;
 	MongoOperations mongoOps;
 
 	public ReceptionService() { // constractor なので void不要
 
 	}
 
-	public void insertVisitor( String org, String name, Integer number ) {
+	public void insertVisitor(String org, String name, Integer number) {
 		//
 		DbConnection();
 		//
@@ -32,14 +32,23 @@ public class ReceptionService {
 	// ----------------------------------------------------------
 	// add record.
 	// ----------------------------------------------------------
-	public void DBinsert(String name, String org, Integer number) {
+	public void DBinsert(String name, String org, Integer num) {
 		LocalDateTime date = LocalDateTime.now();
 		LocalDateTime epoch = LocalDateTime.of(2000, 1, 1, 0, 0, 0);
 
 		System.out.println("LocalDate" + date);
 
 		//
-		mongoOps.insert(new OfficeVisit(name, org, number, date, "", epoch));
+		OfficeVisit officeVisit = new OfficeVisit();
+
+		officeVisit.setVisitor_name(name);
+		officeVisit.setVisitor_org(org);
+		officeVisit.setVisitor_count(num);
+		officeVisit.setVisited_at(date);
+		officeVisit.setPerson_to_visit("");
+		officeVisit.setLeft_at(epoch);
+
+		mongoOps.insert(officeVisit);
 
 		System.out.println("===== レコード追加 =====");
 		System.out.println(name);
